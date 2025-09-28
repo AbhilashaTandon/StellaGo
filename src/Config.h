9574f6af543b231f9bf6ea0c1d4f89cb6cbd36c7
Abhilasha Tandon <abhilashatandon167@gmail.com>
2025-09-28 02:50:31 -0400

    Added more global variables to config

--- src/Config.h
+++ src/Config.h
@@ -1,2 +1,8 @@
-#define DEBUG false
-#define PROFILE true
+#define DEBUG true
+#define PROFILE false
+
+static constexpr auto BOARD_SIZE = 5;
+
+static constexpr auto komi = 7.5f;
+
+static constexpr auto NUM_POINTS = (BOARD_SIZE + 2) * (BOARD_SIZE + 2);
