6b03cc00ff72c32f0d350913157adb86be339cd1
Abhilasha Tandon <abhilashatandon167@gmail.com>
2025-11-25 02:27:31 -0500

    Removed board size cli arg and added depth cli arg

--- src/main.cpp
+++ src/main.cpp
@@ -11,38 +11,11 @@ int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
-        std::cerr << "Error: program must be started by specifying the board size and side to play. Format your command like so\n\nstella.exe (board size) (b or w for black or white) (optional flags)\n\n";
-        return -1;
-    }
-    std::string board_size_arg = argv[1];
-    int board_size = 19;
-    try
-    {
-        board_size = std::stoi(board_size_arg);
-    }
-    catch (const std::invalid_argument &e)
-    {
-        std::cerr << "Error: " << board_size_arg << " is not a valid board size\n\n";
-        return -1;
-    }
-    if (board_size < 5)
-    {
-        std::cerr << "Error: Board size must be at least 5.\n\n";
-
-        return -1;
-    }
-    if (board_size > 19)
-    {
-        std::cerr << "Error: Board size must be at most 19.\n\n";
-        return -1;
-    }
-    if (board_size % 2 == 0)
-    {
-        std::cerr << "Error: Board size must be an odd number.\n\n";
+        std::cerr << "Error: program must be started by specifying the side to play and search depth. Format your command like so\n\nstella.exe (b or w for black or white) (search depth) (optional flags)\n\n";
         return -1;
     }
 
-    std::string side_arg = argv[2];
+    std::string side_arg = argv[1];
     bool side_to_play = true;
     if (side_arg == "w")
     {
@@ -50,11 +23,52 @@ int main(int argc, char *argv[])
     }
     else if (side_arg != "b")
     {
-        std::cerr << "Error: second argument must be either 'b' or 'w', specifying which side the bot is to play.\n\n";
+        std::cerr << "Error: first argument must be either 'b' or 'w', specifying which side the bot is to play.\n\n";
         return -1;
     }
 
-    std::cout << "Board size " << board_size << " side " << side_to_play << "\n\n";
+    std::string depth_arg = argv[2];
+    int depth = 1;
+    try
+    {
+        depth = std::stoi(depth_arg);
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << "Error: " << depth_arg << " is not a valid depth\n\n";
+        return -1;
+    }
+    if (depth < 1)
+    {
+        std::cerr << "Error: Depth must be at least 1.\n\n";
+
+        return -1;
+    }
+
+    std::cout << "Side " << side_to_play << " depth " << depth << "\n\n";
+
+    Bot bot = Bot();
+
+    int play_count = 0;
+
+    while (bot.get_game_result() == EMPTY)
+    {
+        if (side_to_play == (play_count % 2 == 0))
+        {
+            int move = bot.make_best_play(depth);
+            // print move
+        }
+        else
+        {
+            // read move from stdin
+            // if stdin is "quit" then exit
+            // if stdin is "pass" then pass
+            // if stdin is "resign" then resign
+        }
+        // print board
+    }
+
+    // game loop
 }
 // {
 //     Bot a = Bot();
